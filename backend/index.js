5b4de3c1e34ddf2153f4f22f407f01ac17a893c0
mrshivamshaw <shivamshaw9005@gmail.com>
2025-03-27 21:26:54 +0530

    added frontend url

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin:["http://localhost:5173"],
+        origin:["http://localhost:5173","https://copy-evaluation.vercel.app"],
         methods:["POST","GET","PUT","DELETE"],
         credentials:true
     }
